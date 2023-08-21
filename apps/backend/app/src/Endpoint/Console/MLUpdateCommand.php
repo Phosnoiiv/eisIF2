d1bca93b2f404e13be3232a832f4fe26816859ae
Phosnoiiv <68527268+Phosnoiiv@users.noreply.github.com>
2023-08-21 22:52:00 +0800

    [ML][Backend] Add console command to trigger update manually

--- apps/backend/app/src/Endpoint/Console/MLUpdateCommand.php
+++ apps/backend/app/src/Endpoint/Console/MLUpdateCommand.php
@@ -4,22 +4,36 @@ namespace EverISay\SIF\Backend\Endpoint\Console;
 use EverISay\SIF\Backend\Endpoint\Web\Feed\MLFeedController;
 use EverISay\SIF\ML\Storage\UpdateStorage;
 use Spiral\Boot\EnvironmentInterface;
+use Spiral\Console\Attribute\Argument;
 use Spiral\Console\Attribute\AsCommand;
 use Spiral\Console\Command;
 use Symfony\Component\Process\Process;
 
 #[AsCommand('ml:update', 'Update SIFML game data on a single machine')]
 final class MLUpdateCommand extends Command {
+    #[Argument]
+    private ?string $hash = null;
+    #[Argument]
+    private ?string $time = null;
+
     public function __invoke(
         EnvironmentInterface $env,
         UpdateStorage $updateStorage,
         MLFeedController $mLFeedController,
     ): int {
-        $currentHash = rtrim($this->runUpdater($env, ['a'])->getOutput());
         $infoMetadata = $updateStorage->readMetadata();
         $previousHash = reset($infoMetadata);
-        if ($currentHash == $previousHash) return self::SUCCESS;
-        $this->runUpdater($env, ['du', $currentHash]);
+        if (empty($this->hash)) {
+            $currentHash = rtrim($this->runUpdater($env, ['a'])->getOutput());
+            if ($currentHash == $previousHash) return self::SUCCESS;
+            $this->runUpdater($env, ['du', $currentHash]);
+        } else {
+            if ($this->hash == $previousHash) return self::SUCCESS;
+            $this->runUpdater($env, array_merge(
+                ['du', $this->hash, '-m'],
+                !empty($this->time) ? ['-t', $this->time] : [],
+            ));
+        }
         $mLFeedController->update();
         $mLFeedController->updateInsider();
         $mLFeedController->updateMusic();
